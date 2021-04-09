dc48b460df37f8a18f843cd711056c1c70b9a330
shahedzaman-web <physics.shahedzaman@gmail.com>
2021-04-09 22:44:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require("express");
+
+const bodyParser = require("body-parser");
+
+const cors = require("cors");
+
+const port = 5000;
+
+const admin = require("firebase-admin");
+
+require("dotenv").config();
+// console.log(process.env.DB_USER);
+// console.log(process.env.DB_PASS);
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+var serviceAccount = require("./firebase-adminsdk.json");
+
+const MongoClient = require("mongodb").MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kljii.mongodb.net/burjAlArab?retryWrites=true&w=majority`;
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: process.env.FIRE_DB,
+});
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const bookings = client.db("burjAlArab").collection("bookings");
+
+  app.post("/addBooking", (req, res) => {
+    const newBooking = req.body;
+    bookings.insertOne(newBooking).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  app.get("/bookings", (req, res) => {
+    const bearer = req.headers.authorization;
+    if (bearer && bearer.startsWith("Bearer ")) {
+      const idToken = bearer.split(" ")[1];
+      admin
+        .auth()
+        .verifyIdToken(idToken)
+        .then(function (decodedToken) {
+          const tokenEmail = decodedToken.email;
+          const queryEmail = req.query.email;
+          if (tokenEmail == queryEmail) {
+            bookings.find({ email: queryEmail }).toArray((err, documents) => {
+              res.status(200).send(documents);
+            });
+          } else {
+            res.status(401).send("un-authorized access");
+          }
+        })
+        .catch(function (error) {
+          res.status(401).send("un-authorized access");
+        });
+    } else {
+      res.status(401).send("un-authorized access");
+    }
+  });
+});
+
+app.listen(port);
